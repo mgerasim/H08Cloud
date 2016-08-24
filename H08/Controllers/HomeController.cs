004477df3270ed830372fabcda2c011c08b43be9
Mikhail Gerasimov <Gerasimov@dvugms.khv.ru>
2016-08-24 14:00:02 +1000

    init

---
+++ H08/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace H08.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            H08.Models.H08 theH08 = new H08.Models.H08();
+
+            ViewBag.Base64 = theH08.base64String;
+
+            return View();
+        }
+
+    }
+}
